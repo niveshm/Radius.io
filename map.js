0607c6d15e74c3308409a6927ddff9989d8e5208
Nivesh-02 <47472803+Nivesh-02@users.noreply.github.com>
2020-02-01 10:35:02 +0530

    Update map.js

--- map.js
+++ map.js
@@ -121,32 +121,7 @@ window.onload = () => {
             icon.addEventListener('loaded', () => {
                 window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'));
             });
-
+            scene.appendChild(icon);
         });
-
-        const clickListener = function(ev) {
-            ev.stopPropagation();
-            ev.preventDefault();
-
-            const name = ev.target.getAttribute('name');
-
-            const el = ev.detail.intersection && ev.detail.intersection.object.el;
-
-            if (el && el === ev.target) {
-                const label = document.createElement('span');
-                const container = document.createElement('div');
-                container.setAttribute('id', 'place-label');
-                label.innerText = name;
-                container.appendChild(label);
-                document.body.appendChild(container);
-
-                setTimeout(() => {
-                    container.parentElement.removeChild(container);
-                }, 1500);
-            };
-        };
-        icon.addEventListener('click', clickListener);
-
-        scene.appendChild(icon);
     });
 };
